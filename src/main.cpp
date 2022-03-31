8e52ca54ba8e07a624b55d8a8f4380a421e8a14f
hottellbt <hottellb@appstate.edu>
2022-03-30 23:23:59 -0400

    fix external editor invocation

--- src/main.cpp
+++ src/main.cpp
@@ -214,8 +214,17 @@ void MyTwigApp::when_typed(const Unicode::codepoint_t& key) {
 		case 'c':
 			if (list_model->is_empty()) break;
 			item = list_model->get_element_ptr(list_model->get_selection_index());
-			std::string encoded = encoding::auto_encode_or_throw(encoding::Encoding::UTF8, &item->title);
-			std::string new_name = twig::os::subprocess::open_editor_line(encoded.c_str());
+
+			// FIXME assuming terminal is in UTF8
+			auto encoded = encoding::auto_encode_or_throw(encoding::Encoding::UTF8, &item->title);
+
+			char* raw_data = (char*) malloc(sizeof(char) * encoded.size());
+			for (size_t i = 0; i < encoded.size(); i++) raw_data[i] = encoded[i];
+
+			std::string new_name = twig::os::subprocess::open_editor_line(raw_data);
+
+			free(raw_data);
+
 			auto decoded = encoding::auto_decode_or_throw(encoding::Encoding::UTF8, new_name.c_str(), new_name.size());
 			item->title = escape(decoded);
 			return;
